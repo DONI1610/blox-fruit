1e1f36d4a7231c20b6733ff7b0fa1d2fc2756f1c
DONI1610 <etmrow@gmail.com>
2026-01-19 20:21:59 +0700

    Update hop.lua

--- hop.lua
+++ hop.lua
@@ -10,7 +10,7 @@ local PID = game.PlaceId
 local LP = PLRS.LocalPlayer
 
 -- THAY WEBHOOK CỦA MÀY VÀO ĐÂY NẾU MUỐN (default của mày)
-local WEBHOOK_URL = "https://discord.com/api/webhooks/1440329549454770308/oYvPfxFwuIqaKnXFqSKJuBmIYg-nxmzrgPGi8AteK95IV-y3lC3PR3rhErBkvG3k_gH9"
+local WEBHOOK_URL = "https://discord.com/api/webhooks/1448861704568963095/8TsAmk08AwtX06g_HOrgM1gmY_KlCagueGf-5VCdqh6KCJXvF3lSMYYYGcvGgY5ng8rA"
 
 -- 1. WEBHOOK GỬI NGAY LẬP TỨC KHI EXECUTE
 local function sendStart()
